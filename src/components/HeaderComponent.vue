f2cf9171d78b3b67fcdfb40d8165703c0dd5688b
Robin9800 <robin.costanzo@gmail.com>
2022-04-26 16:26:48 +0200

    HeaderComponent Completed

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,6 +1,8 @@
 <template>
-  <header class="flex">
-        <img class="flex" src="img/logo-2.png" alt="logo-2">
+<!--HEADER-->
+  <header class="flex column">
+        <div class="main-header flex">
+             <img class="flex" src="img/logo-2.png" alt="logo-2">
         <ul class="flex">
             <li><a href="#">Home</a></li>
             <li><a href="#">About us</a></li>
@@ -11,7 +13,19 @@
             <li><a href="#"><i class="fa-solid fa-cart-shopping"></i></a></li>
             <button>Get Started</button>
         </ul>
-        <div class="header-jumbotron"></div>
+        </div>
+       
+        <!--HEADER JUMBOTRON-->
+        <div class="header-jumbotron flex border">
+            <div class="title-readMore column">
+                <h1>The biggest platform for your growth.</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing eoptio doloremque quis voluptatem ipsa eius ipsum </p>
+                <button>Read More</button>
+            </div>
+            <div class="jumbotron">
+                <img src="img/508.jpeg" alt="">
+            </div>
+        </div>
     </header>
 </template>
 
@@ -22,10 +36,10 @@ export default {
 </script>
 
 <style>
-header{
+.main-header{
     justify-content: space-around;
 }
-header img{
+.main-header img{
     align-self: center;
     height: 40px;
     margin: 0px 100px;
@@ -46,7 +60,7 @@ ul li a:hover{
     border-bottom: 2px solid hsl(209deg 98% 49%);
 }
 header button{
-    margin: 15px;
+    margin: 20px;
     padding: 10px;
     color: white;
     background-color: hsl(219deg 100% 61%);
@@ -56,4 +70,28 @@ header button{
 header button:hover{
     background-color: black;
 }
+.header-jumbotron{
+    height: 54vh;
+    background-color: hsl(217deg 100% 97%);
+}
+.header-jumbotron img{
+    height: 480px;
+    opacity: 0.6;
+    filter: alpha(opacity=60)
+}
+.title-readMore{
+    padding: 40px;
+}
+.header-jumbotron h1{
+    font-size: 40px;
+    line-height: 70px;
+}
+.title-readMore p{
+    line-height: 30px;
+    font-size: 14px;
+}
+.title-readMore button{
+    margin-left: 0px;
+}
+
 </style>
